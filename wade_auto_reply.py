b04d3f732149e417d2e1a6788121efde1207c53a
MinGyu Jeon <33545415+mkjyong@users.noreply.github.com>
2024-11-26 14:43:24 +0900

    Feat : conversable agent example

--- wade_auto_reply.py
+++ wade_auto_reply.py
@@ -1,5 +1,5 @@
 import os
-from autogen import AssistantAgent, UserProxyAgent
+from autogen import ConversableAgent
 
 os.environ['OPENAI_API_KEY'] = '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'
 
@@ -12,48 +12,21 @@ llm_config = {
     ]
 }
 
-
-# 템플릿 기반 프롬프트
-prompt_template = """
-당신은 장난꾸러기야. 입력된 데이터를 기반으로 놀리거나 장난을 쳐줘. 100자 이내로.
-
-데이터: {data}
-"""
-
-
 # 에이전트 생성
-assistant = AssistantAgent(
-    name="WADE_NAUGHTY",
-    llm_config=llm_config
+cathy = ConversableAgent(
+    "cathy",
+    system_message="Your name is Cathy and you are a part of a duo of comedians.",
+    llm_config=llm_config,
+    human_input_mode="NEVER",  # Never ask for human input.
 )
 
-user_proxy = UserProxyAgent("user_proxy", code_execution_config=False)
+joe = ConversableAgent(
+    "joe",
+    system_message="Your name is Joe and you are a part of a duo of comedians.",
+    llm_config=llm_config,
+    human_input_mode="NEVER",  # Never ask for human input.
+)
 
+result = joe.initiate_chat(cathy, message="Cathy, tell me a joke.", max_turns=2)
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
-# FastAPI 앱 생성
-app = FastAPI()
-
-
-class UserInput(BaseModel):
-    user_input: str
-
-@app.post("/process")
-async def process_input(input_data: UserInput):
-    # 사용자 요청 처리
-    user_message = prompt_template.format(
-        data=input_data.user_input
-    )
-
-    # 프롬프트 실행
-    response = user_proxy.initiate_chat(
-        assistant,
-        message=user_message,
-        auto_reply=True,  # 사용자 입력 없이 자동으로 진행
-        human_input_mode="auto"  # 사용자 입력 없이 자동 응답
-    )
-
-    print("aaa", response)
-    return {"response": response}
+print(result)
